e933cf747bac18a1226fe5c45e4e79cf1e2c5462
jason <jasonedlewis@gmail.com>
2019-11-15 03:53:44 -0500

    added post actions and updated post reducer

--- src/redux/actions/PostActions.js
+++ src/redux/actions/PostActions.js
@@ -1,1 +1,5 @@
 import { REQUEST_POST, POST_SUCCESS, POST_FAILURE, CREATE_POST , EDIT_POST , DELETE_POST } from './types'
+
+const fetchPost =()=> dispatch =>{
+    
+}
---
+++ src/redux/reducers/PostReducer.js
@@ -0,0 +1,35 @@
+import { REQUEST_POST, POST_SUCCESS, POST_FAILURE, CREATE_POST, EDIT_POST, DELETE_POST } from './types'
+
+const initialState = {
+    post: [],
+    request: null,
+    success: null,
+    errorMessage: ""
+}
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case REQUEST_POST:
+            return { ...state, request: true }
+        case POST_SUCCESS:
+            return {
+                ...state, request: false, success: true,
+                post: action.payload
+            }
+        case POST_FAILURE:
+            return { ...state, success: false, request: false, errorMessage: action.payload }
+        case CREATE_POST:
+            return {
+                ...state, request: false, success: true,
+                post: [...state.post, action.payload]
+            }
+        case EDIT_POST:
+            return {
+                ...state,
+
+            }
+
+        default:
+            break;
+    }
+
+}
