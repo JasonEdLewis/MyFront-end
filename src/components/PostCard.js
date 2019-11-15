28a10f3ffa3280d8be0eb80564c18ebae97a8e2f
jason <jasonedlewis@gmail.com>
2019-11-14 19:02:32 -0500

    recontructing post card

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -19,32 +19,32 @@ const PostCard = props => {
     } else {
       return <h6 style={{ color: "light-grey" }}>Be the first to comment</h6>;
     }
-
-
   };
-  const userNameOfPost = () => {
-   return props.map(post => console.log(post))
-
-  }
+ 
   return (
     <div className="post-card-div">
-      <Card id={post.id} className="post-card">
-        <Image
+      <div id={post.id} className="post-card">
+        <div className="card-header">
+        {/* REPLACE WITH THE IMAGE ASSOCIATED WITH NAME
+        <img
           className="post-thumbnail"
           src={require("../img/jack.jpg")}
-        /> <button id="add-friend">friend</button>
+        />  */}
+        <button id="add-friend" onClick={()=> {console.log(post.user.id)}}>🤝</button>
 
-        <span className="span-style">{post.user.username}</span>
+        <span className="name-span-style" onClick={()=> {console.log(post.user.id)} }>{post.user.username}</span>
+        </div>
 
-        <Card.Img className="img-box" src={require('../img/pic_placeholder.png')} />
-
-        <Card.Body>
-          <Card.Title></Card.Title>
-          <Card.Text>
-            <h5 className="caption">User: {post.caption}</h5>
-            <ul className='ulStyle'>{comment()}</ul>
-          </Card.Text>
-          <Form.Control
+        <div className="img-div">
+        <img  src={require('../img/pic_placeholder.png')} />
+        </div>
+        
+         
+           
+          <div className="comments-div">
+          <h5 className="caption">{post.user.username}: {post.caption}</h5>
+          <ul className='ulStyle'>{comment()}</ul>
+          <input
             size="sm"
             type="text"
             name="comment"
@@ -52,49 +52,18 @@ const PostCard = props => {
             onChange={props.handleComment}
             placeholder="comment"
           />
-        </Card.Body>
-        <Button variant="link" onClick={props.submitComment}>
+          </div>
+            
+       
+        
+      
+        <span variant="link" onClick={props.submitComment} id="post-span">
           Post
-      </Button>
-      </Card>
+      </span>
+      </div>
+     
     </div>
   );
 };
 
 export default PostCard;
-
-
-// const divStyle = {
-//   marginLeft: "32%",
-//   marginRight: "auto",
-//   width: "18rem",
-//   color: "black"
-// };
-// const cardStyle = {
-//   width: "450px",
-//   hieght: "250px",
-//   marginLeft: "auto",
-//   marginRight: "auto",
-//   border: "1px",
-//   borderStyle: "dashed",
-//   borderColor: "pink",
-//   backgroundColor: "#fff"
-// };
-// const spanStyle = {
-//   marginLeft: "auto",
-//   marginRight: "auto",
-//   marginTop: "0%",
-//   color: "pink",
-//   fontStyle: "italic",
-//   fontWeight: "bold",
-//   fontSize: "185%"
-// };
-// const thumbnailStyle = {
-//   height: "50px",
-//   marginRight: "5%",
-//   marginTop: "3%"
-// };
-// const ulStyle = {
-//   listStyleType: "square",
-//   color: "black"
-// };
