2e7954cb67fa28b6a21d63c6671969c3831aa562
jason <jasonedlewis@gmail.com>
2020-01-01 17:19:09 -0500

    When cloning disengage persist until after first login

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,25 @@
-import { createStore, applyMiddleware,compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage' ;
+import storage from 'redux-persist/lib/storage';
 
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
 
 
 const persistConfig = {
-    key: 'root',
-    storage,
-  }
+  key: 'root',
+  storage,
+}
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-   export const store = createStore(persistedReducer, {}, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    ));
-    export const persistor = persistStore(store);
-    
-export default {store , persistor }
+export const store = createStore(persistedReducer, {}, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+));
+export const persistor = persistStore(store);
+
+// export default {store }
+
+export default { store, persistor }
 
 
 
