eb11f19ec5b5ca07b01389c1b2f86fb7a5871aa1
jason <jasonedlewis@gmail.com>
2020-01-02 17:53:46 -0500

    stopped persistor

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import { store, persistor }from './redux/store';
+import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
 
 
 
 ReactDOM.render(
- 
+
 
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-     
-    </PersistGate>
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <App />
+
+    {/* </PersistGate> */}
   </Provider>
-    ,document.getElementById('root'));
+  , document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
