982323ad32f9effebf9900f6b55b2a0ec8bae40d
jason <jasonedlewis@gmail.com>
2020-01-02 18:35:12 -0500

    trying a build

---
+++ build/precache-manifest.a40f89f32d410d12103e54baf9af6b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9e47b5992dd306332a2de8a415574f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a38f8b06baf6d7d02f0",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c3bab2b5f924e275cf43",
+    "url": "/static/css/main.343683a2.chunk.css"
+  },
+  {
+    "revision": "2a38f8b06baf6d7d02f0",
+    "url": "/static/js/2.ee187ce1.chunk.js"
+  },
+  {
+    "revision": "7cc162655caf3844ab3d76e223a6e5a7",
+    "url": "/static/js/2.ee187ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3bab2b5f924e275cf43",
+    "url": "/static/js/main.641cab59.chunk.js"
+  },
+  {
+    "revision": "03357149483a376b5b2e",
+    "url": "/static/js/runtime-main.b09effc8.js"
+  }
+]);
