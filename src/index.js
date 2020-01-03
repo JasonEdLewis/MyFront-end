528ef71b20ccd486dbf2c43c9e97e879205fcb18
jason <jasonedlewis@gmail.com>
2020-01-03 00:35:25 -0500

    fixed the heroku foolishness

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ ReactDOM.render(
 
 
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
 
-    {/* </PersistGate> */}
+    </PersistGate>
   </Provider>
   , document.getElementById('root'));
 
